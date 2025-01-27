65ef6810442cc71516c98d5daa6ea820a5577996
vinod-kumar1 <vinu22149@gmail.com>
2025-01-27 06:52:15 +0530

    added 404

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDom from "react-dom/client";
 import App from "./app";
 import { BrowserRouter } from "react-router";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 let root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={isDevelopment ? "/" : "Spotlight-with-react"}>
     <App />
   </BrowserRouter>
 );
